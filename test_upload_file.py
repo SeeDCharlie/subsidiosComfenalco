0da247961f8e92572217daa67179fbe962b7db1f
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-04-23 12:38:33 -0500

    implementacion de la carga de archivos

--- test_upload_file.py
+++ test_upload_file.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'https://comfenalcosubsidios.seedcharlie.co/uploadFile.php'
 files = {'uploaded_file': open('/home/seed/Imágenes/fp.jpg','rb')}
-values = {'id': '333', 'uploadForm':True}
+values = {'id_subsidio': 88,'idProgRequerimiento': 8,'guardarEvidencia':True}
 
 r = requests.post(url, files=files, data=values)
 print(r)
