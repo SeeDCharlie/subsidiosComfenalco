af1d45cc96337b5b6364c064f99f690de440960c
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-04-23 12:47:22 -0500

    iplemntacion de la carga de archivos

---
+++ src/Services/AnexosService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Usuarios;
+use DateTime;
+use Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\UsuariosRepository;
+
+
+class AnexosService
+{
+
+    
+    
+
+}
--- test_upload_file.py
+++ test_upload_file.py
@@ -1,8 +1,15 @@
 import requests
 
 url = 'https://comfenalcosubsidios.seedcharlie.co/uploadFile.php'
-files = {'uploaded_file': open('/home/seed/Imágenes/fp.jpg','rb')}
-values = {'id_subsidio': 88,'idProgRequerimiento': 8,'guardarEvidencia':True}
 
-r = requests.post(url, files=files, data=values)
+#archivo que se va a cargar
+files = {'uploaded_file': open('/home/seed/Imágenes/fp.jpg','rb')}
+
+#valores para la carga de un requerimiento de un subcidio
+valuesForRequirement = {'id_subsidio': 88,'idProgRequerimiento': 8,'guardarEvidencia':True}
+
+#valores para la carga de un formulario de inscripcion 
+valuesForForm = {'id_subsidio': 88,'guardarEvidencia':True}
+
+r = requests.post(url, files=files, data=valuesForRequirement)
 print(r)
