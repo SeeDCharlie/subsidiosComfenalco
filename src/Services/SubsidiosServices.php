c20a54c1a76fa4a6420b422161343f43acf6699f
SeeDCharlie <hellocarloscastillo@gmail.com>
2021-04-29 15:30:40 -0500

    asdasd

--- src/Services/SubsidiosServices.php
+++ src/Services/SubsidiosServices.php
@@ -12,25 +12,34 @@ class SubsidiosServices
 
 
 
-    public function registrarSubsidio($dats, $em)
+    public function registrarSubsidio($request, $em, $nameFileDirectory)
     {
 
         $response = new JsonResponse();
-        $soliSubsidio = new Subsidios();
+        
 
         try {
+
+            $soliSubsidio = new Subsidios();
+
+            $dats = json_decode($request->getContent(), true);
+
             $soliSubsidio->setIdEstado(1);
             $soliSubsidio->setIdUsuario($dats['idUsr']);
             $soliSubsidio->setIdPrograma($dats['idPrograma']);
             $soliSubsidio->setFechaCreacion(new DateTime(date("Y-m-d")));
             $soliSubsidio->setFechaModificacion(new DateTime(date("Y-m-d")));
             $soliSubsidio->setFechaFinalizacion(new DateTime($dats['fechaFinalizacion']));
-            
+            $soliSubsidio();
 
             $em->persist($soliSubsidio);
             $em->flush();
 
-            $soliSubsidio->setFormulario("uploads/formularioInscripcion/".$soliSubsidio->getIdSubsidios()."_".$dats['nombreArchivo']);
+            $file = $request->files->get('uploaded_file');
+            $fileName = $soliSubsidio->getIdSubsidios().'form_'.new DateTime(date("Y-m-d")). '.' . $file->guessExtension();
+            $file->move($nameFileDirectory, $fileName);
+
+            $soliSubsidio->setFormulario($nameFileDirectory.$fileName);
 
             $em->flush();
 
