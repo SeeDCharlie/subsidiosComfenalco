7d7372c4600882b9fd40dd19f4072e8e30ff7977
seed <seed@seedpc.seedhost>
2021-06-06 16:50:56 -0500

    guardar notificacion

--- src/Controller/NotiSoliController.php
+++ src/Controller/NotiSoliController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\NotiSoli;
 use \Doctrine\ORM\EntityManager;
 use App\Repository\NotificacionesRepository;
 use App\Repository\NotiSoliRepository;
+use App\Repository\SubsidiosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,6 +20,28 @@ use Symfony\Component\Validator\Constraints\Json;
 class NotiSoliController extends AbstractController{
 
 
+    /**
+     * @Route("/getAllNotiASoli", name="getAllNotiASoli", methods = {"GET"})
+     * 
+     */    
+    public function getAllNotiASoli(Request $request, NotiSoliRepository $notiSoliRepository):JsonResponse {
+
+        try {
+            $noti = $notiSoliRepository->findAll();
+
+            if(!$noti){
+                return new JsonResponse( "NO hay notificaciones" , Response::HTTP_BAD_GATEWAY);
+            }else{
+                $data = $this->get('serializer')->serialize($noti, 'json');
+
+                return new JsonResponse(json_decode($data, true), Response::HTTP_OK);
+            }
+        } catch (Exception $error) {
+            return new JsonResponse("error : $error", Response::HTTP_BAD_GATEWAY);
+        }        
+
+    }
+
 
     /**
      * @Route("/notiSoliPorId", name="notiSoliPorId", methods = {"GET"})
@@ -26,20 +50,54 @@ class NotiSoliController extends AbstractController{
     public function notiSoliPorId(Request $request, NotiSoliRepository $notiSoliRepository):JsonResponse {
 
         try {
-            
             $id = $request->query->get('id');
-
             $noti = $notiSoliRepository->find($id);
-
             if(!$noti){
                 return new JsonResponse( "NO hay notificaciones con el id : $id" , Response::HTTP_BAD_GATEWAY);
             }else{
                 $data = $this->get('serializer')->serialize($noti, 'json');
-
                 return new JsonResponse(json_decode($data, true), Response::HTTP_OK);
+            }
+        } catch (Exception $error) {
+            return new JsonResponse("error : $error", Response::HTTP_BAD_GATEWAY);
+        }        
+
+    }
+
+      /**
+     * @Route("/guardarNotiSoli", name="guardarNotiSoli", methods = {"POST"})
+     * 
+     */    
+    public function guardarNotiSoli(Request $request, EntityManagerInterface $em, SubsidiosRepository $subRepository, NotificacionesRepository $notifRepository, NotiSoliRepository $notiSoliRepository):JsonResponse {
+
+        try {
+            $dats = json_decode($request->getContent(), true);
+
+            $noti = $notifRepository->find($dats['idNoti']);
+            $subsidio = $subRepository->find($dats['idSubsidio']);
+
+            if(!$noti){
+                return new JsonResponse( "NO hay notificaciones con el id : ".$dats['idNoti'] , Response::HTTP_BAD_GATEWAY);
+            }
+            if(!$subsidio){
+                return new JsonResponse( "NO hay subsidios con el id : ".$dats['idSubsidio'] , Response::HTTP_BAD_GATEWAY);
+            }
+            if(empty($dats['mensaje'])){
+                return new JsonResponse( "NO esta definido el mesaje".$dats['mensaje'] , Response::HTTP_BAD_GATEWAY);
             }
 
 
+            $noti_soli = new NotiSoli();
+            $noti_soli->setIdNotificacion($dats['idNoti']);
+            $noti_soli->setIdSubsidios($dats['idSubsidio']);
+            $noti_soli->setMensaje($dats['mensaje']);
+
+            $em->persist($noti_soli);
+            $em->flush();
+
+
+            return new JsonResponse("noti soli guardada", Response::HTTP_OK);
+            
         } catch (Exception $error) {
             return new JsonResponse("error : $error", Response::HTTP_BAD_GATEWAY);
         }        
